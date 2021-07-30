3baeaaf65eb2b99cce61b0d1288e8e67dbfd74fe
Ebube111 <ebubeagwaze@gmail.com>
2021-07-30 13:36:21 +0100

    new commits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import styled from "styled-components";
+
+const Form = styled.form`
+  border: 1px solid red;
+  width: 500px;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const FormItem = styled.div`
+  margin: 10px 0;
+`;
+
+const Button = styled.div`
+  width: 100px;
+  height: 30px;
+  margin-left: 110px;
+`;
+
+const Error = styled.p`
+  color: red;
+  font-size: 12px;
+`;
+export const ValidateInput = (str = " ") => str.includes("@");
 
 function App() {
+  const [formData, setFormData] = useState({});
+
+  const handleOnChange = ({ target: { name, value } }) =>
+    setFormData(prev => ({ ...prev, [name]: value }));
+
+  const handleSubmit = async () => {
+    const response = await fetch("https://dummyapi.io/data/api/");
+    try {
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <Form name="login-form" onSubmit={handleSubmit}>
+          <FormItem>
+            <label htmlFor="email">Email: </label>
+            <input id="email" name="email" onChange={handleOnChange} />
+          </FormItem>
+          {formData.email && !ValidateInput(formData.email) ? (
+            <Error>Email not valid</Error>
+          ) : null}
+          <FormItem>
+            <label htmlFor="password">Password: </label>
+            <input id="password" name="password" onChange={handleOnChange} />
+          </FormItem>
+          <Button role="button" onClick={handleSubmit}>
+            submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent } from "@testing-library/react";
+import React from "react";
+import App, { ValidateInput, handleSubmit } from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("login", () => {
+  test("validate function should pass on correct input", () => {
+    const text = "text@text.com";
+    expect(ValidateInput(text)).toBe(true);
+  });
+
+  test("email input should accept a text", () => {
+    const { getByLabelText, getByText } = render(<App />);
+    const emailInputNode = getByLabelText("Email:");
+    expect(emailInputNode.value).toMatch("");
+    fireEvent.change(emailInputNode, { target: { value: "testing" } });
+    expect(emailInputNode.value).toMatch("testing");
+
+    const errorMessageNode = getByText("Email not valid");
+    expect(errorMessageNode).toBeInTheDocument();
+
+    fireEvent.change(emailInputNode, { target: { value: "testing@" } });
+    expect(errorMessageNode).not.toBeInTheDocument();
+  });
+
+  // MAIN TASK TO DO
+  test("should fail when the api is called", () => {
+    const { getByRole } = render(<App />);
+    const buttonNode = getByRole("button");
+    fireEvent.submit(buttonNode);
+    expect(buttonNode).toThrow(
+      "SyntaxError: Unexpected token A in JSON at position 0"
+    );
+  });
 });
